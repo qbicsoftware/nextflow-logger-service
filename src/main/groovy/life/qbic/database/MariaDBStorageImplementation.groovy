07e342c49f2a4e3df4a3ba75cf319e7040d2e86d
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2019-04-04 15:40:35 +0200

    Uses Datasource injection now

--- src/main/groovy/life/qbic/database/MariaDBStorageImplementation.groovy
+++ src/main/groovy/life/qbic/database/MariaDBStorageImplementation.groovy
@@ -1,68 +1,32 @@
 package life.qbic.database
 
 import groovy.sql.Sql
-import io.micronaut.context.annotation.Property
-import io.micronaut.context.annotation.Requires
 import life.qbic.nextflow.WeblogMessage
 import life.qbic.nextflow.weblog.Trace
 
+import javax.inject.Inject
 import javax.inject.Singleton
+import javax.sql.DataSource
 import java.sql.Connection
-import java.sql.DriverManager
 
 @Singleton
-@Requires(property = "app.db.host")
-@Requires(property = "app.db.port")
-@Requires(property = "app.db.name")
-@Requires(property = "app.db.pw")
-@Requires(property = "app.db.user")
-@Requires(property = "app.db.driver.class")
-@Requires(property = 'app.db.driver.prefix')
 class MariaDBStorageImplementation implements WeblogStorage{
 
-    private Connection connection
+    private DataSource dataSource
 
-    private String databaseUrl
-
-    private DatabaseProperties databaseProperties
-
-    MariaDBStorageImplementation(@Property(name = 'app.db.host') String databaseHost,
-                                 @Property(name = 'app.db.port') String databasePort,
-                                 @Property(name = 'app.db.name') String databaseName,
-                                 @Property(name = 'app.db.user') String databaseUser,
-                                 @Property(name = 'app.db.pw') String userPassword,
-                                 @Property(name = 'app.db.driver.class') String driverClass,
-                                 @Property(name = 'app.db.driver.prefix') String driverPrefix) {
-        databaseProperties = new DatabaseProperties().tap {
-            it.databaseName = databaseName
-            it.databasePort = databasePort
-            it.databaseHost = databaseHost
-            it.driverPrefix = driverPrefix
-            it.driverClass = driverClass
-        }
-
-        buildDatabaseUrlFromProperties()
-        createDatabaseConnectionWithCredentials(databaseUser, userPassword)
-    }
-
-    private void buildDatabaseUrlFromProperties(){
-        databaseUrl = databaseProperties.getDatabaseConnectionUrl()
-    }
-
-    private void createDatabaseConnectionWithCredentials(String user, String password) throws Exception {
-        Class.forName(databaseProperties.driverClass)
-        connection = DriverManager.getConnection(databaseUrl, user, password)
+    @Inject MariaDBStorageImplementation(DataSource dataSource) {
+        this.dataSource = dataSource
     }
 
     void storeWeblogMessage(WeblogMessage message) throws WeblogStorageException{
         try {
-            tryToStoreWeblogMessage(message)
+            tryToStoreWeblogMessage(message, dataSource.connection)
         } catch (Exception e) {
             throw new WeblogStorageException("Could not store weblog message: $message", e)
         }
     }
 
-    private void tryToStoreWeblogMessage(WeblogMessage message) {
+    private void tryToStoreWeblogMessage(WeblogMessage message, Connection connection) {
         final def sql = new Sql(connection)
         insertRunInfo(message, sql)
         insertTraceInfo(message.trace, sql)
@@ -75,29 +39,5 @@ class MariaDBStorageImplementation implements WeblogStorage{
 
     private void insertTraceInfo(Trace trace, Sql sql) {
         //TODO Implement trace info insertion
-    }
-
-    private static class DatabaseProperties {
-
-        private String databasePort, databaseHost, databaseName, driverPrefix, driverClass
-
-        private String getUrl() {
-            "${driverPrefix}://${databaseHost}:${databasePort}/${databaseName}"
-        }
-
-        private String getUrlWithPort() {
-            "${driverPrefix}://${databaseHost}/${databaseName}"
-        }
-
-        String getDatabaseConnectionUrl() {
-            final String url
-            if (databasePort) {
-                url = getUrlWithPort()
-            } else {
-                url = getUrl()
-            }
-            return url
-        }
-
     }
 }
