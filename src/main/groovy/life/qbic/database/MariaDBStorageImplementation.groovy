724160e3805c8ee2cfc0863a15c1de75633bf1ac
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2019-04-23 09:40:42 +0200

    Introduces simple run info insertion

--- src/main/groovy/life/qbic/database/MariaDBStorageImplementation.groovy
+++ src/main/groovy/life/qbic/database/MariaDBStorageImplementation.groovy
@@ -1,7 +1,11 @@
 package life.qbic.database
 
 import groovy.sql.Sql
+import io.micronaut.context.annotation.Requires
+import io.micronaut.context.annotation.Value
 import life.qbic.nextflow.WeblogMessage
+import life.qbic.nextflow.weblog.MetaData
+import life.qbic.nextflow.weblog.RunInfo
 import life.qbic.nextflow.weblog.Trace
 
 import javax.inject.Inject
@@ -10,31 +14,50 @@ import javax.sql.DataSource
 import java.sql.Connection
 
 @Singleton
+@Requires(property='database.name', defaultValue="workflows")
+@Requires(property='database.tables.runs', defaultValue="runs")
+@Requires(property='database.tables.traces', defaultValue="traces" )
 class MariaDBStorageImplementation implements WeblogStorage{
 
     private DataSource dataSource
+
+    @Value('${database.name}')
+    private static String WF_DATABASE_NAME
+
+    @Value('${database.tables.runs}')
+    private static String WF_RUNS_TABLE
+
+    @Value('${database.tables.traces}')
+    private static String WF_TRACES_TABLE
 
     @Inject MariaDBStorageImplementation(DataSource dataSource) {
         this.dataSource = dataSource
     }
 
     void storeWeblogMessage(WeblogMessage message) throws WeblogStorageException{
+        final def sql = new Sql(dataSource.connection)
         try {
-            tryToStoreWeblogMessage(message, dataSource.connection)
+            tryToStoreWeblogMessage(message, sql)
+            sql.close()
         } catch (Exception e) {
+            sql.close()
             throw new WeblogStorageException("Could not store weblog message: $message", e)
         }
     }
 
-    private void tryToStoreWeblogMessage(WeblogMessage message, Connection connection) {
-        final def sql = new Sql(connection)
-        insertRunInfo(message, sql)
+    private void tryToStoreWeblogMessage(WeblogMessage message, Sql sql) {
+        insertRunInfo(message.runInfo, sql)
         insertTraceInfo(message.trace, sql)
-        sql.close()
+        insertMetadataInfo(message.metadata, sql)
     }
 
-    private void insertRunInfo(WeblogMessage, Sql sql) {
-        //TODO Implement run info insertion
+    private void insertMetadataInfo(MetaData metaData, Sql sql) {
+        //TODO Implement metadata insertion
+    }
+
+
+    private void insertRunInfo(RunInfo runInfo, Sql sql) {
+        sql.execute("insert into ${WF_RUNS_TABLE} (runId) values ($runInfo.id)")
     }
 
     private void insertTraceInfo(Trace trace, Sql sql) {
