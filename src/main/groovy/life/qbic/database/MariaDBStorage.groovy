b77f1b4eb9e9f20fa6d67993b12ad4c47866c978
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2019-06-25 09:48:00 +0200

    More verbose logging for dates

--- src/main/groovy/life/qbic/database/MariaDBStorage.groovy
+++ src/main/groovy/life/qbic/database/MariaDBStorage.groovy
@@ -255,6 +255,8 @@ class MariaDBStorage implements WeblogStorage, AutoCloseable{
         log.info(timestamp)
         Date parsedDate = databaseDateFormat.parse(timestamp)
         log.info(parsedDate)
+        log.info(parsedDate.class)
+        log.info(parsedDate.format(Constants.ISO_8601_DATETIME_FORMAT))
         return parsedDate.format(Constants.ISO_8601_DATETIME_FORMAT)
     }
 
