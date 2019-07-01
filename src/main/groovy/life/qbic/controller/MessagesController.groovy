9295f88c88eef0b52296bf24be5780fc0066cbd8
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2019-07-01 13:20:45 +0200

    Hopefully includes stack trace now

--- src/main/groovy/life/qbic/controller/MessagesController.groovy
+++ src/main/groovy/life/qbic/controller/MessagesController.groovy
@@ -81,7 +81,6 @@ class MessagesController {
             metaData = informationCenter.getMetadataOfWorkflow(runId)
         } catch( Exception e ) {
             log.error(e)
-            log.debug(e.printStackTrace())
             return HttpResponse.serverError()
         }
         metaData ? HttpResponse.ok(metaData) : HttpResponse.notFound(metaData)
